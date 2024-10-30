457fe29e17bd78ff80e104f06d1b584149c6968e
Biplab Sutradhar <biplabsutradhar1453@gmail.com>
2024-10-30 23:50:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { gql, useQuery } from "@apollo/client";
+import './App.css'; // Import the CSS file
+
+const query = gql`
+  query GetTodosWithUser {
+    getTodos {
+      id
+      title
+      completed
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
 
 function App() {
+  const { data, loading } = useQuery(query);
+
+  if (loading) return <h1 className="loading-text">Loading...</h1>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1 className="header">Todo List</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="th">Title</th>
+            <th className="th">User</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.getTodos.map((todo) => (
+            <tr key={todo.id} className="row">
+              <td className="td">{todo.title}</td>
+              <td className="td">{todo?.user?.name || "Unknown"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
